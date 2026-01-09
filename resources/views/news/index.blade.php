d248c2c1a7e68d1121995245fa4fcafe26bfdf10
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2026-01-09 10:08:49 +0600

    feat: Enhance 'Next Page' link with SVG icon and improved styling in Blog and News views

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -29,10 +29,16 @@
 				</div>
 			</article>
 		@endforeach
-		<div class="d-flex justify-content-end">
-			@if ($next_page != null)
-				<a href="{{ url()->current() }}?page={{ $next_page }}" class="next-page align-self-end">Next Page</a>
-			@endif
-		</div>
+		@if ($next_page != null)
+			<div class="d-flex justify-content-end">
+				<a href="{{ url()->current() }}?page={{ $next_page }}" class="next-page align-self-end">
+					Next Page
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" width="16" height="16" fill="currentColor">
+						<path
+							d="M471.1 297.4C483.6 309.9 483.6 330.2 471.1 342.7L279.1 534.7C266.6 547.2 246.3 547.2 233.8 534.7C221.3 522.2 221.3 501.9 233.8 489.4L403.2 320L233.9 150.6C221.4 138.1 221.4 117.8 233.9 105.3C246.4 92.8 266.7 92.8 279.2 105.3L471.2 297.3z" />
+					</svg>
+				</a>
+			</div>
+		@endif
 	@endempty
 @endsection
